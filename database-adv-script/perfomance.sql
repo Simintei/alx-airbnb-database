cc80f6bb6566c5827c52dbb92c63ea1315186f08
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-09-04 10:32:05 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,4 @@
-Write an initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
+--Write an initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
 
 
 SELECT
@@ -36,6 +36,43 @@ JOIN
 JOIN
     property AS p ON b.property_id = p.property_id
 
+-- Join with the payment table on booking_id to get payment information
+JOIN
+    payment AS pay ON b.booking_id = pay.booking_id;
+
+EXPLAIN
+SELECT
+    -- Booking Details
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status AS booking_status,
+    b.created_at AS booking_created_at,
+
+    -- User Details
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+
+    -- Property Details
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+
+    -- Payment Details
+    pay.payment_id,
+    pay.amount AS payment_amount
+
+FROM
+    booking AS b
+-- Join with the users table on user_id to get user information
+JOIN
+    users AS u ON b.user_id = u.user_id
+-- Join with the property table on property_id to get property information
+JOIN
+    property AS p ON b.property_id = p.property_id
 -- Join with the payment table on booking_id to get payment information
 JOIN
     payment AS pay ON b.booking_id = pay.booking_id;
